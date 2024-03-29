0db9d27f10c34e66af155fb7192229695c3660d6
AdrianRuchala <adrianruchala03@gmail.com>
2024-03-29 12:17:47 +0100

    0.3.11 - Konfiguracja Firebase Admin SDK na backendzie

---
+++ E-Dziekanat_Server/composeexample/firebase.py
@@ -0,0 +1,17 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+
+cred = credentials.Certificate("e-dziekanat-4e60f-firebase-adminsdk-t7grn-01e3972a35.json")
+firebase_admin.initialize_app(cred)
+
+# Dodawanie użytkownika testowe
+#email = input('Wprowadź email: ')
+#password = input('Wprowadź hasło: ')
+#user = auth.create_user(email = email, password = password)
+#print("Użytkownik został stworzony: {0}".format(user.uid))
+
+# Usuwanie użytkownike testowe
+uid = input('Wprowadź UID użytkownika: ')
+auth.delete_user(uid)
+print("Użytkownik został usunięty")
