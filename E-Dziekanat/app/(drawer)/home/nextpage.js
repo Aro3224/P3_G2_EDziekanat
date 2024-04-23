0d27c805b798704414353933595c5bc36a191b09
wolskikarol <wolskikarol22@gmail.com>
2024-04-23 16:25:51 +0200

    0.5.3 - odpowiadanie na powiadomienia

--- E-Dziekanat/app/(drawer)/home/nextpage.js
+++ E-Dziekanat/app/(drawer)/home/nextpage.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Button, FlatList } from 'react-native';
 import { Drawer } from 'expo-router/drawer';
 import { DrawerToggleButton } from '@react-navigation/drawer';
 import { db } from '../../../components/configs/firebase-config';
-import { ref, get, child, set } from "firebase/database";
+import { ref, get, child, set, push, serverTimestamp, onValue } from "firebase/database";
 import { useRoute } from '@react-navigation/native';
 import { auth } from '../../../components/configs/firebase-config';
 
 export default function NextPage() {
   const [notificationContent, setNotificationContent] = useState('');
   const [notificationTitle, setNotificationTitle] = useState('');
+  const [response, setResponse] = useState('');
+  const [responses, setResponses] = useState([]);
   const route = useRoute();
   const notificationId = route.params.id; // Przechwyć przekazane id wiadomości
 
@@ -51,20 +53,73 @@ export default function NextPage() {
       }
     };
 
+
+    const fetchResponses = () => {
+      const dbRef = ref(db, `notifications/${auth.currentUser.uid}/${notificationId}/odpowiedzi`);
+      onValue(dbRef, (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+          const responsesArray = Object.values(data);
+          setResponses(responsesArray);
+        } else {
+          setResponses([]);
+        }
+      });
+    };
+
     fetchNotificationContent();
     fetchNotificationTitle();
     updateReadStatus();
+    fetchResponses();
   }, [notificationId]);
+
+  const handleSendResponse = async () => {
+    try {
+      const dbRef = ref(db, `notifications/${auth.currentUser.uid}/${notificationId}/odpowiedzi`);
+      const responseObj = {
+        tresc: response,
+        data: serverTimestamp()
+      };
+      await push(dbRef, responseObj);
+      setResponse('');
+      // Możesz także dodać logikę do wyświetlenia potwierdzenia wysłania odpowiedzi
+    } catch (error) {
+      console.error('Błąd podczas zapisywania odpowiedzi:', error);
+    }
+  };
+
+  const renderResponseItem = ({ item }) => (
+    <View style={styles.responseItem}>
+      <Text>{item.tresc}</Text>
+      <Text>Data przesłania: {new Date(item.data).toLocaleString()}</Text>
+    </View>
+  );
+
 
   return (
     <View style={styles.container}>
-      <Drawer.Screen
+       <Drawer.Screen
         options={{
-          title: notificationTitle, // Ustaw tytuł strony na tytuł wiadomości
+          title: notificationTitle,
           headerShown: true,
         }}
       />
-      <Text>{notificationContent}</Text>
+      <Text style={styles.title}>{notificationTitle}</Text>
+      <Text style={styles.content}>{notificationContent}</Text>
+      <FlatList
+        data={responses}
+        renderItem={renderResponseItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setResponse}
+        value={response}
+        placeholder="Wpisz odpowiedź"
+        multiline={true}
+        numberOfLines={4}
+      />
+      <Button title="Wyślij" onPress={handleSendResponse} />
     </View>
   );
 }
@@ -73,7 +128,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  }
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  content: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+  responseItem: {
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
 });
