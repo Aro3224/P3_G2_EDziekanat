cff19ec1963a9da1d5d4715298c6db17eeeeaa75
AdrianRuchala <adrianruchala03@gmail.com>
2024-06-23 19:45:43 +0200

    buildowanie apki za pomocą electrona

---
+++ E-Dziekanat/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const fs = require('fs');
+const packageJsonPath = './package.json';
+const packageJson = require(packageJsonPath);
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1440,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'node_modules/expo-router/entry.js')
+    }
+  });
+
+  packageJson.main = 'expo-router/entry';
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+  mainWindow.loadURL('http://localhost:8081');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
